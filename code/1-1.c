99168fdd84d073f08ed1b8e7ebcb39ca39768955
Kanyon05 <windtree01@gmail.com>
2026-04-24 11:31:26 +0900

    Simplify printf statements in main function

--- code/1-1.c
+++ code/1-1.c
@@ -5,9 +5,9 @@ int main() {
     char s2[] = "hi";
     char s3[] = {'h', 'i', '\0'};
 
-    printf("s1: %s\n", s1);
-    printf("s2: %s\n", s2);
-    printf("s3: %s\n", s3);
+    printf("%s\n", s1);
+    printf("%s\n", s2);
+    printf("%s\n", s3);
 
     return 0;
 }
